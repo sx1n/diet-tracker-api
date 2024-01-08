f0f5fc87d0c382a25d8397ca39a4f2fd05bb3ede
sx1n <samuellaurindodelima2004@gmail.com>
2024-01-08 02:23:06 -0300

    feat: login

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,70 @@
+import { hash } from "bcrypt";
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+
+import { AuthenticateUseCase } from "./authenticate";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: AuthenticateUseCase;
+
+describe("Authenticate Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new AuthenticateUseCase(usersRepository);
+  });
+
+  it("should be able to autheticate", async () => {
+    await usersRepository.create({
+      firstname: "John",
+      lastname: "Doe",
+      gender: "M",
+      height: 175,
+      weight: 70.5,
+      physicalActivity: "Moderate",
+      weightGoal: 65.0,
+      birthdate: new Date("1990-05-15"),
+      email: "john.doe@example.com",
+      passwordHash: await hash("secretpassword", 6),
+    });
+
+    const { user } = await sut.execute({
+      email: "john.doe@example.com",
+      password: "secretpassword",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to authenticate with wrong email", async () => {
+    await expect(() =>
+      sut.execute({
+        email: "john.doe@example.com",
+        password: "secretpassword",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it("should not be able to authenticate with wrong password", async () => {
+    await usersRepository.create({
+      firstname: "John",
+      lastname: "Doe",
+      gender: "M",
+      height: 175,
+      weight: 70.5,
+      physicalActivity: "Moderate",
+      weightGoal: 65.0,
+      birthdate: new Date("1990-05-15"),
+      email: "john.doe@example.com",
+      passwordHash: await hash("secretpassword", 6),
+    });
+
+    await expect(() =>
+      sut.execute({
+        email: "john.doe@example.com",
+        password: "wrongpassword",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+});
---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,40 @@
+import { compare } from "bcrypt";
+
+import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
+
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+
+interface AuthenticateUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User;
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await compare(password, user.passwordHash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      user,
+    };
+  }
+}
